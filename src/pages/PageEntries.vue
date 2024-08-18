a02227af86e5ad30dc2588ceba9f012854b6b245
Роман Бушуев <bushuev@benefitty.ru>
2024-08-18 20:30:08 +0400

    feat: add Entries Page Design

--- src/pages/PageEntries.vue
+++ src/pages/PageEntries.vue
@@ -1,9 +1,110 @@
 <template>
-  <q-page class="flex flex-center">
-    Entries
+  <q-page>
+    <div class="q-pa-md">
+      <q-list
+        bordered
+        separator
+      >
+        <q-item
+          v-for="entry in entries"
+          :key="entry.id"
+        >
+          <q-item-section
+            class="text-weight-bold"
+            :class="useAmountColorClass(entry.amount)"
+          >
+            {{ entry.name }}
+          </q-item-section>
+          <q-item-section
+            class="text-weight-bold"
+            :class="useAmountColorClass(entry.amount)"
+            side
+          >
+            {{ useCurrency(entry.amount) }}
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </div>
+
+    <q-footer
+      class="bg-transparent"
+    >
+      <div class="row q-mb-sm q-px-md q-py-sm shadow-up-3">
+        <div class="col text-grey-7 text-h6">
+          Balance:
+        </div>
+        <div
+          :class="useAmountColorClass(balance)"
+          class="col text-h6 text-right"
+        >
+          {{ useCurrency(balance) }}
+        </div>
+      </div>
+      <div class="row q-px-sm q-pb-sm q-col-gutter-sm bg-primary">
+        <div class="col">
+          <q-input
+            placeholder="Name"
+            bg-color="white"
+            outlined
+            dense
+          />
+        </div>
+        <div class="col">
+          <q-input
+            input-class="text-right"
+            placeholder="Amount"
+            bg-color="white"
+            type="number"
+            step="0.01"
+            outlined
+            dense
+          />
+        </div>
+        <div class="col col-auto">
+          <q-btn
+            color="primary"
+            icon="add"
+            round
+          />
+        </div>
+      </div>
+    </q-footer>
+
   </q-page>
 </template>
 
 <script setup>
+import { ref, computed } from 'vue'
+import { useCurrency } from 'src/use/useCurrency'
+import { useAmountColorClass } from 'src/use/useAmountColorClass'
 
+
+const  entries =  ref([
+  {
+    id: 'id1',
+    name: 'Salary',
+    amount: 4999.99
+  },
+  {
+    id: 'id2',
+    name: 'Rent',
+    amount: -999
+  },
+  {
+    id: 'id3',
+    name: 'Phone',
+    amount: -14.99
+  },
+  {
+    id: 'id4',
+    name: 'Unknown',
+    amount: 0
+  },
+])
+
+const balance = computed(() => {
+  return entries.value.reduce((acc, { amount }) => {
+    return acc + amount
+  }, 0)
+})
 </script>
